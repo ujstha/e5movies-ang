4a07cbbb7ecd8a9f8a058ec749dbabbc1cfa4742
ujstha <ujstha27@gmail.com>
2018-10-14 23:46:46 +0300

    some update on MovieSchema

--- models/Movie.js
+++ models/Movie.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 
-var BookSchema = new mongoose.Schema({
+var MovieSchema = new mongoose.Schema({
   title: String,
   cast: String,
   genre: String,
   storyline: String,
-  release_date: String,
+  release_date: Date,
   director: String,
   movie_poster: String,
   updated_date: { type: Date, default: Date.now },
