ef39c26a4853baba70de25b918fe88a6844e2718
ujstha <ujstha27@gmail.com>
2018-10-15 03:51:29 +0300

    movie routes finished

--- routes/movie.js
+++ routes/movie.js
@@ -11,4 +11,36 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET SINGLE MOVIE BY ID */
+router.get('/:id', function(req, res, next) {
+  Movie.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
 
+/* SAVE MOVIE */
+router.post('/', function(req, res, next) {
+  Movie.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* UPDATE MOVIE */
+router.put('/:id', function(req, res, next) {
+  Movie.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE MOVIE */
+router.delete('/:id', function(req, res, next) {
+  Movie.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
