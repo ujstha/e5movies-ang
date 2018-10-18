59cb5ffa373087e666c471f4f1969eed5de93f13
ujstha <ujstha27@gmail.com>
2018-10-18 06:33:30 +0300

    bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,7 @@ import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   {
-    path: '/',
+    path: './',
     component: HomeComponent,
     data: { title: 'Home' }
   },
@@ -64,7 +64,7 @@ const appRoutes: Routes = [
     data: { title: 'About Us' }
   },
   { path: '',
-    redirectTo: '/',
+    redirectTo: './',
     pathMatch: 'full'
   }
 ];
